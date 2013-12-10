aaee1c4e866c76b6374cebb7599e1c01a3ab2754
A. N <apockalipss@hotmail.fr>
2013-12-10 13:49:08 -0800

    Added basic LUA Implementation (Load script, register functions, events)

---
+++ Editor/Editor/Editor/Game/Script/CLuaVM.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NLua;
+
+namespace Editor.Game.Script
+{
+    class CLuaVM : CScriptVM
+    {
+        public Lua VMHandler;
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            VMHandler = new Lua();
+            VMHandler.LoadCLRPackage();
+
+            // Initialize Events
+           RegisterFunction("addEvent", this, this.GetType().GetMethod("internal_AddEvent"));
+        }
+
+        public override void internal_AddEvent(string eventName, string functionVMName)
+        {
+            base.internal_AddEvent(eventName, functionVMName);
+        }
+
+        public override void RegisterFunction(string functionName, object target, System.Reflection.MethodBase function)
+        {
+            VMHandler.RegisterFunction(functionName, target, function);
+        }
+
+        public override void LoadScript(string scriptName)
+        {
+            try
+            {
+                VMHandler.DoFile(scriptName);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public override void CallEvent(string eventName, object[] parameters = default(object[]))
+        {
+            if (EventsListVM.ContainsKey(eventName))
+            {
+                LuaFunction eventFunc = (LuaFunction)VMHandler["onPlayerDieEvent"];
+                eventFunc.Call(parameters);
+            }
+        }
+    }
+}
---
+++ Editor/Editor/Editor/Game/Script/CScriptVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using NLua;
+
+namespace Editor.Game.Script
+{
+    abstract class CScriptVM
+    {
+        public Dictionary<string, string> EventsListVM = new Dictionary<string, string>();
+
+        public abstract void LoadScript(string scriptName);
+        public abstract void RegisterFunction(string functionName, object target, System.Reflection.MethodBase function);
+        public abstract void CallEvent(string eventName, object[] parameters);
+
+        public virtual void Initialize()
+        {
+        }
+
+        public virtual void internal_AddEvent(string eventName, string functionVMName)
+        {
+            if (!EventsListVM.ContainsKey(eventName))
+            {
+                EventsListVM.Add(eventName, functionVMName);
+            }
+        }
+    }
+}
