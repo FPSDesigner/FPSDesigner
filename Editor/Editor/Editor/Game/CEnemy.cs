619cf7984dc87bc722b89863f80888641f89a914
Peicho David <dadouvic2@hotmail.fr>
2014-05-04 11:16:08 -0700

    Test

--- Editor/Editor/Editor/Game/CEnemy.cs
+++ Editor/Editor/Editor/Game/CEnemy.cs
@@ -14,15 +14,18 @@ namespace Engine.Game
     class CEnemy
     {
         //float _life;
+        private Texture2D[] _modelTextures; //ALl the texture storaged in an array
         private Display3D.MeshAnimation _model; //The 3Dmodel and all animations
 
         public void LoadContent(ContentManager content)
         {
+            _modelTextures = new Texture2D[1];
+            _modelTextures[0] = content.Load<Texture2D>("Textures\\M40A5");
             _model = new Display3D.MeshAnimation("StormTrooper", 1, 1, 1.0f, new Vector3(-165.2928f, 169f, 85f),
-                Matrix.CreateRotationX(-1 * MathHelper.PiOver2), 0.55f, null, 10, 0.0f, true);
+                Matrix.CreateRotationX(-1 * MathHelper.PiOver2), 0.55f, _modelTextures, 10, 0.0f, true);
 
             _model.LoadContent(content);
-            _model.BeginAnimation("test", false);
+            _model.BeginAnimation("crouch", true);
         }
 
         public void Update(GameTime gameTime)
