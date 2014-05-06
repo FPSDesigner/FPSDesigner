f8bb3fe1caeea16cb13cbe553a1f8a6fef667993
A. N <apockalipss@hotmail.fr>
2014-05-06 15:19:33 -0700

    Hide white pixel

--- Editor/Editor/Editor/Display2D/C2DEffect.cs
+++ Editor/Editor/Editor/Display2D/C2DEffect.cs
@@ -295,7 +295,7 @@ namespace Engine.Display2D
                 _spriteBatch.Draw(_fadeTexture, _fadePositionRect, null, new Color(_fadeToColor.R, _fadeToColor.G, _fadeToColor.B, _fadeOpacity/255), 0f, Vector2.Zero, _fadeSizeRect, SpriteEffects.None, 0);
             }
 
-            _spriteBatch.Draw(_pixelTexture, new Rectangle(_graphicsDevice.Viewport.Width / 2, _graphicsDevice.Viewport.Height / 2, 4, 4), Color.White);
+            //_spriteBatch.Draw(_pixelTexture, new Rectangle(_graphicsDevice.Viewport.Width / 2, _graphicsDevice.Viewport.Height / 2, 4, 4), Color.White);
 
         }
 
